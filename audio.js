7ecd3b9ea958f84ce80f0a30fd4cf0abd00d07a7
Ansub Khan <khanansub3@gmail.com>
2021-12-29 21:26:46 +0530

    used onClick() to run audio

---
+++ audio.js
@@ -0,0 +1,71 @@
+function rain() {
+  const myAudio = document.getElementById("rain");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function cafe() {
+  const myAudio = document.getElementById("cafe");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function city() {
+  const myAudio = document.getElementById("city");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function beach() {
+  const myAudio = document.getElementById("beach");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function thunder() {
+  const myAudio = document.getElementById("thunder");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function breeze() {
+  const myAudio = document.getElementById("breeze");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function bird() {
+  const myAudio = document.getElementById("bird");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
+
+function night() {
+  const myAudio = document.getElementById("night");
+  if (myAudio.paused) {
+    myAudio.play();
+  } else {
+    myAudio.pause();
+  }
+}
