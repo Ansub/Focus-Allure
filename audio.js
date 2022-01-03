b8f2615fd727167c773aea2f6b25120c2dcbf271
Ansub Khan <khanansub3@gmail.com>
2022-01-03 16:55:17 +0530

    fixed button clicked error, added breeze audio

--- audio.js
+++ audio.js
@@ -4,11 +4,13 @@ const sounds = {
   city: "audio/city.mp3",
   beach: "audio/beach.mp3",
   thunder: "audio/thunder.mp3",
+  breeze: "audio/breeze.mp3",
   bird: "audio/bird.mp3",
   night: "audio/night.mp3",
 };
 
 document.querySelector("#sound-btns").addEventListener("click", function (e) {
+  console.log(e.target.id);
   for (let sound of Object.keys(sounds)) {
     if (e.target.id === sound) {
       let audio = new Audio(sounds[sound]);
